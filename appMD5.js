d5ef052956a0fdbb77bf9e4ab1e60cd31f087763
HuangQingyi <hqingyicn@foxmail.com>
2018-08-24 11:47:37 +0800

    Update appMD5.js

--- appMD5.js
+++ appMD5.js
@@ -10,7 +10,6 @@ app.get('/', function (req, res) {
   // 如果是 post 传来的 body 数据，则是在 req.body 里面，
   // 不过 express 默认不处理 body 中的信息，
   // 需要引入 https://github.com/expressjs/body-parser 这个中间件才会处理，
-  // 如果分不清什么是 query，什么是 body 的话，那就需要补一下 http 的知识了
   var q = req.query.q;
 
   // 调用 utility.md5 方法，得到 md5 之后的值
@@ -18,6 +17,7 @@ app.get('/', function (req, res) {
   // utility 的 github 地址：https://github.com/node-modules/utility
   // 里面定义了很多常用且比较杂的辅助方法，可以去看看
   var md5Value = utility.md5(q);
+  /*当前star 最多的md5是blueimp*/
 
   res.send(md5Value);
 });
